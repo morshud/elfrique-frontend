8e1204a816efc094f9923bff266561286f95e27a
morshud_lotus <morshudlhgmo@gmail.com>
2022-05-31 11:21:20 +0100

    Dashbaord Data

--- src/service/getDashboard.js
+++ src/service/getDashboard.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-//const API_URL = "https://elfrique-proj.herokuapp.com/api/v1/";
-const API_URL = "http://localhost:3000/api/v1/";
+const API_URL = "https://elfrique-proj.herokuapp.com/api/v1/";
+//const API_URL = "http://localhost:3000/api/v1/";
 
 class GetDashboard {
   getDashboard(id) {
